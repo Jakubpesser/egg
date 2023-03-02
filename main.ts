ea1f4c651eb97f3f4a634adee1165d84382e2e3c
Jakubpesser <115145147+Jakubpesser@users.noreply.github.com>
2023-03-02 11:17:25 +0100

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,26 @@
-input.setAccelerometerRange(AcceleratorRange.TwoG)
+// input.setAccelerometerRange(AcceleratorRange.TwoG)
 
-basic.forever(function() {
-    let x = input.acceleration(Dimension.X)
-    let y = input.acceleration(Dimension.Y)
-    let z = input.acceleration(Dimension.Z)
-    let s = input.acceleration(Dimension.Strength)
-    console.logValue("x", x)
-    console.logValue("y", y)
-    console.logValue("z", z)
-    basic.pause(100)
+// basic.forever(function() {
+//     let x = input.acceleration(Dimension.X)
+//     let y = input.acceleration(Dimension.Y)
+//     let z = input.acceleration(Dimension.Z)
+//     let s = input.acceleration(Dimension.Strength)
+//     console.logValue("x", x)
+//     console.logValue("y", y)
+//     console.logValue("z", z)
+//     basic.pause(100)
+// })
+
+
+input.onButtonPressed(Button.A, function () {
+    ExpSense.restart()
+}) 
+
+ExpSense.onExplosiveState(function(xaxis: number, yaxis: number, shakingvalue: number) {
+    console.logValue("x", xaxis)
+    console.logValue("y", yaxis)
 })
-input.onButtonPressed(Button.A, function() {
-    
+
+ExpSense.onExplode(function() {
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
 })
